c53ec678010fcd1254700d92dcc6214a5322f8f0
sam stearns <sam.stearns@gmail.com>
2022-03-14 17:59:23 -0400

    Update postgres_load_data.sql

--- nh/postgres_load_data.sql
+++ nh/postgres_load_data.sql
@@ -4,3 +4,8 @@ copy public."REF_HCG" FROM '/path/to/file/PUBLICUSE_REF_TABLES/REF_HCG.txt' DELI
 
 # Claims Tables
 copy public."CLAIM_MC" FROM '/path/to/file/2019.txt' DELIMITER '|' NULL AS '';
+
+-- Provider file
+-- NOTE: This had invalid characters
+-- iconv -f windows-1252 -t UTF-8 PUBLICUSE_PROVIDER_DETAIL.txt > provider
+-- tr < provider -d '\000' > p2
